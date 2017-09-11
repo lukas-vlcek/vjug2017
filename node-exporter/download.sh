1158c0f49fb1c302d240c66306841b8219a21b5c
Lukas Vlcek <lukas.vlcek@gmail.com>
2017-09-11 15:38:15 +0200

    Add node-exporter

---
+++ node-exporter/download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#wget https://github.com/prometheus/node_exporter/releases/download/v0.14.0/node_exporter-0.14.0.darwin-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v0.14.0/node_exporter-0.14.0.linux-amd64.tar.gz
+
+echo "tar -xvzf node_exporter..."
