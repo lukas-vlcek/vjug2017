2a2fd1960e81f206b1b9888c05ad2f2b3f3b07f5
Lukáš Vlček <lukas.vlcek@gmail.com>
2017-09-12 10:51:16 +0200

    Make download script more autonomous

--- node-exporter/download.sh
+++ node-exporter/download.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-#wget https://github.com/prometheus/node_exporter/releases/download/v0.14.0/node_exporter-0.14.0.darwin-amd64.tar.gz
-wget https://github.com/prometheus/node_exporter/releases/download/v0.14.0/node_exporter-0.14.0.linux-amd64.tar.gz
+uname -a
 
-echo "tar -xvzf node_exporter..."
+arch=`getconf LONG_BIT`
+if [ "${arch}" != "64" ]; then
+  echo "Unsupported architecture"
+  exit;
+fi
+
+os_type=`uname`
+os_type="$(tr [A-Z] [a-z] <<< "$os_type")"
+
+url=https://github.com/prometheus/node_exporter/releases/download/v0.14.0/node_exporter-0.14.0.${os_type}-amd64.tar.gz;
+
+if [ "linux" != "${os_type}" ] && [ "darwin" != "${os_type}" ]; then
+  echo "${os_type}" is not supported ATM
+  exit
+fi
+
+wget ${url}
+
+echo "Extract archive:"
+echo "  tar -xvzf node_exporter..."
